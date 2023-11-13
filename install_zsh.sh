a7b09bd946824ae7b82ff132aa78031e303a0714
Oliver0804 <icetzsr@gmail.com>
2023-11-13 13:02:19 +0800

    Update install_zsh.sh
    1.add echo
    2.關閉下載字體，有需求自行開啟

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,37 +1,52 @@
 #!/bin/bash
 
-# 安裝必要的套件
-sudo apt install wget git curl vim zsh -y
+# Update and upgrade system packages
+echo -e "\e[31mUpdating and upgrading system packages...\e[0m"
+apt update -y
+apt upgrade -y
 
-# 下載並安裝 MesloLGS NF 字型
-wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf
-wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf
-wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf
-wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf
+# Install required packages
+echo -e "\e[31mInstalling required packages: wget, git, curl, vim, zsh...\e[0m"
+apt install wget git curl vim zsh -y
 
-# 安裝 Zsh
-sudo apt install zsh -y
+# Download and install MesloLGS NF fonts (commented out)
+#echo -e "\e[31mDownloading and installing MesloLGS NF fonts...\e[0m"
+#wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Regular.ttf
+#wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold.ttf
+#wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Italic.ttf
+#wget https://github.com/romkatv/dotfiles-public/raw/master/.local/share/fonts/NerdFonts/MesloLGS%20NF%20Bold%20Italic.ttf
 
-# 安裝 Oh My Zsh
+# Install Zsh
+echo -e "\e[31mInstalling Zsh...\e[0m"
+apt install zsh -y
+
+# Install Oh My Zsh
+echo -e "\e[31mInstalling Oh My Zsh...\e[0m"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# 設定預設 Shell 為 Zsh
+# Set Zsh as default shell
+echo -e "\e[31mSetting Zsh as the default shell...\e[0m"
 chsh -s $(which zsh)
 
-# 安裝 PowerLevel10k 主題
+# Install PowerLevel10k theme
+echo -e "\e[31mInstalling PowerLevel10k theme...\e[0m"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
-# 安裝插件
+# Install plugins
+echo -e "\e[31mInstalling plugins: zsh-autosuggestions, zsh-syntax-highlighting, zsh-z...\e[0m"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 
-# 修改 .zshrc 配置
+# Modify .zshrc configuration
+echo -e "\e[31mModifying .zshrc configuration...\e[0m"
 sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
 sed -i 's/^plugins=(.*)$/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-z)/' ~/.zshrc
 
-# 應用 .zshrc 的更改
+# Apply .zshrc changes
+echo -e "\e[31mApplying changes to .zshrc...\e[0m"
 source ~/.zshrc
 
-# 配置 Powerlevel10k
+# Configure Powerlevel10k
+echo -e "\e[31mConfiguring Powerlevel10k...\e[0m"
 p10k configure
